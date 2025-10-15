7e0272ff02ec28bf8e5b78483464d36f6d747bd6
Andy <63891246+and-eee@users.noreply.github.com>
2025-10-15 19:27:10 +1100

    Initial upload
    Uploading initial files

---
+++ classes/includes/till-payments-provider.php
@@ -0,0 +1,21 @@
+<?php
+
+final class WC_TillPayments_Provider
+{
+    public static function paymentMethods()
+    {
+        /**
+         * Comment/disable adapters that are not applicable
+         */
+        return [
+            'WC_TillPayments_CreditCard',
+//            'WC_TillPayments_GooglePay',
+//            'WC_TillPayments_ApplePay',
+        ];
+    }
+
+    public static function autoloadClient()
+    {
+        require_once TILL_PAYMENTS_EXTENSION_BASEDIR . 'classes/vendor/autoload.php';
+    }
+}
